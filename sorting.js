19af0894d871863977b76b353b39334995a8fb42
sammymarks <sammy.marks@gmail.com>
2024-01-26 10:20:18 -0600

    bucket sort complete

--- sorting.js
+++ sorting.js
@@ -1,17 +1,19 @@
 // Sorting Questions - https://www.techiedelight.com/sorting-interview-questions/
 
-
-//bubble sort
-
-const bubbleSort = (size) => {
-
+const randomArray = (size) => {
     let rand = []
     for (let i=0; i<size; i++) {
         rand.push(Math.floor(Math.random()*100))
     }
+    return rand
+}
 
-    console.log("rand", rand)
 
+//bubble sort
+
+const bubbleSort = (arr) => {
+    // console.log("BUBBLE SORT")
+    // console.log(arr)
 
     //
 
@@ -21,62 +23,57 @@ const bubbleSort = (size) => {
     while (repeat == true) {
         let flipCount = 0
 
-        for (let i=0; i<(rand.length-1); i++) {
+        for (let i=0; i<(arr.length-1); i++) {
 
-            let one = rand[i]
-            let two = rand[i+1]
+            let one = arr[i]
+            let two = arr[i+1]
 
 
             if (one > two) {
-                rand[i] = two
-                rand[i+1] = one
+                arr[i] = two
+                arr[i+1] = one
                 flipCount++
             }
 
         }
         if (flipCount == 0) {repeat = false}
     }
-    return rand
+    return arr
 }
 
-// console.log(bubbleSort(10))
+// console.log(bubbleSort(randomArray(10)))
 
 //insertion sort
 
-const insertionSort = (size) => {
-    
+const insertionSort = (arr) => {
+    console.log("INSERTION SORT")
+    console.log(arr)
     //create a random array
-    let rand = []
-    for (let i=0; i<size; i++) {
-        rand.push(Math.floor(Math.random()*100))
-    }
-
-    console.log("rand", rand)
 
     //create the output array starting with the first element in rand
     //for each element in rand, find the right place in output and remove from rand
-    let output = [rand[0]]
+    let output = [arr[0]]
     
-    for (let j=1; j<rand.length; j++) {
+    for (let j=1; j<arr.length; j++) {
         // console.log ("current output", output)
-        // console.log(j, rand[j])
+        // console.log(j, arr[j])
         //check special cases (largest and smallest)
         //check each pair and insert at location
-        //special case - rand[0] is the smallest
-        if (rand[j]<output[0]) {
+        //special case - arr[0] is the smallest
+        if (arr[j]<output[0]) {
             console.log("SMALLEST")
-            output.unshift(rand[j])
-        //special case - rand[0] is the largest
-        } else if (rand[j]>output[output.length - 1]) {
+            output.unshift(arr[j])
+        //special case - arr[0] is the largest
+        } else if (arr[j]>output[output.length - 1]) {
             console.log("LARGEST")
-            output.push(rand[j])
+            output.push(arr[j])
         } else {
             console.log("MIDDLE")
-            console.log(rand[j])
+            console.log(arr[j])
             let index
 
             for (let k = 0; k<output.length; k++ ) {
-                if (rand[j]>output[k] && rand[j]<output[k+1]) {
+                if (arr[j]>output[k] && arr[j]<output[k+1]) {
                     console.log("found index")
                     console.log("k", k)
                     console.log("left", output[k])
@@ -86,7 +83,7 @@ const insertionSort = (size) => {
                 }
             }
             console.log(index)
-            output.splice(index+1,0,rand[j])
+            output.splice(index+1,0,arr[j])
 
         }
         
@@ -96,7 +93,7 @@ const insertionSort = (size) => {
     return(output)
 }
 
-// console.log(insertionSort(5))
+// console.log(insertionSort(randomArray(10)))
 
 //BUCKET SORT
 
@@ -113,36 +110,58 @@ const bucketSort = (arr) => {
     
     //get array characteristics
     let numBuckets = Math.floor(Math.sqrt(arr.length))
-    let max = Math.max(...arr)
-    let min = Math.min(...arr)
+    let top = Math.max(...arr)
+    let bottom = Math.min(...arr)
     // console.log(min, max)
-    let range = max-min
+    let range = top-bottom
+    let bucketSize = range/numBuckets
     
-    //define upper ranges for buckets (less than or equal to)
-    //create placeholder buckets
-    let bucketRanges = []
+    //create placeholder buckets and bucket ranges
+    let bucketRanges = {}
     let buckets = []
 
 
+    //define bucket ranges with the key being the value of the bucket index
     for (let i=0; i<numBuckets; i++) {
-        let divider = min + (range*((i+1)/numBuckets))
-        bucketRanges.push(divider)
-        buckets[i] = []
+        bucketRanges[i]={}
+        buckets[i] = [] 
+        if (i==0) {
+            bucketRanges[i].min = bottom
+            bucketRanges[i].max = bottom+bucketSize
+        } else if (i==(numBuckets-1)){
+            bucketRanges[i].min = bucketRanges[i-1].max
+            bucketRanges[i].max = top
+        } else {
+            bucketRanges[i].min = bucketRanges[i-1].max
+            bucketRanges[i].max = bucketRanges[i].min + bucketSize
+        }
     }
 
+    console.log("bucketRanges", bucketRanges)
 
+    //for each array number, loop through the bucket ranges to check the right bucket and push the number in the right bucket
     for (let j=0; j<arr.length; j++) {
-        for (let k=0; k<numBuckets; k++) {
-            if (arr[j]<=bucketRanges[k]) {
-                buckets[k].push(arr[j])
+        //special case
+        if (arr[j]==bottom) {
+            buckets[0].push(arr[j])
+        } else {
+            for (let k=0; k<numBuckets; k++) {
+                if (arr[j]>bucketRanges[k].min && arr[j]<=bucketRanges[k].max) {
+                    buckets[k].push(arr[j])
+                }
             }
         }
     }
 
-    return buckets
+    //sort and concatanate each bucket
+    buckets.forEach(a=>bubbleSort(a))
+    let output = [].concat(...buckets)
+    return output
 }
+
+
 
 console.log(bucketSort([12, 6, 3, 7, 13, 8]))
 console.log(bucketSort([-3, -1, 5, 100]))
-console.log(bucketSort([-3, -1, 5, 52, 100]))
+console.log(bucketSort([-3, -1, 5, 101, 115, 78, 51, 52, 100]))
 
